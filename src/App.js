040983767add8a21737cf07e6519526763cdffed
apta-tamilarasi <athinarayanantamilarasi@gamil.com>
2023-12-31 09:23:02 +0530

    addnew

---
+++ src/App.js
@@ -0,0 +1,10 @@
+
+import './App.css';
+import Functionlifecycle from './Lifecycle/Useeffectparent.js'
+function App() {
+  return (
+    <div><Functionlifecycle/></div>
+  );
+}
+
+export default App;
